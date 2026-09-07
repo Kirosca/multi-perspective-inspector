1d4cb8c2cb602681e3b818897d85a62b526cc8fa
weihao <addpd@qq.com>
2026-09-07 22:07:39 +0800

    fix: resolve icon tree shaking by avoiding dynamic IconData and adding build flag

--- lib/models/profession.dart
+++ lib/models/profession.dart
@@ -44,9 +44,44 @@ class Profession {
       accentColor: json['accentColor'] != null
           ? Color(json['accentColor'] as int)
           : const Color(0xFF2563EB),
-      icon: json['iconCodePoint'] != null
-          ? IconData(json['iconCodePoint'] as int, fontFamily: 'MaterialIcons')
-          : Icons.work_outline,
+      icon: resolveIcon(json['id'] as String?),
     );
+  }
+
+  static IconData resolveIcon(String? id) {
+    switch (id) {
+      case 'police_detective':
+        return Icons.policy_outlined;
+      case 'interior_designer':
+        return Icons.chair_outlined;
+      case 'doctor':
+        return Icons.medical_services_outlined;
+      case 'lawyer':
+        return Icons.gavel_outlined;
+      case 'photographer':
+        return Icons.camera_alt_outlined;
+      case 'architect_engineer':
+        return Icons.foundation_outlined;
+      case 'chef_nutritionist':
+        return Icons.restaurant_outlined;
+      case 'psychologist':
+        return Icons.psychology_outlined;
+      case 'marketing_advertiser':
+        return Icons.campaign_outlined;
+      case 'agronomist':
+        return Icons.eco_outlined;
+      case 'fire_safety_inspector':
+        return Icons.local_fire_department_outlined;
+      case 'geologist_naturalist':
+        return Icons.terrain_outlined;
+      case 'fashion_designer':
+        return Icons.checkroom_outlined;
+      case 'mechanical_engineer':
+        return Icons.precision_manufacturing_outlined;
+      case 'historian_archeologist':
+        return Icons.auto_stories_outlined;
+      default:
+        return Icons.person_search_outlined;
+    }
   }
 }
